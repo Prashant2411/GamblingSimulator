be069c1021d580b1a1ffa125354138d900eafaba
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-11 12:42:01 +0530

    Added: Calculating daily win/loss amount limit

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -5,16 +5,28 @@ echo "Welcome to the Gambling Simulator."
 BET_AMOUNT=1
 WIN=1
 LOSS=0
-STAKE_AMOUNT=100
 
-function getWinLoss () {
-	winLoss=$(( RANDOM%2 ))
-	case $winLoss in
-	$WIN )
-		echo "Win";;
-	$LOSS )
-		echo "Loss";;
-	esac
+stakeAmount=100
+lowerStackLimit=$(( $stakeAmount / 2 ))
+upperStackLimit=$(( $stakeAmount + $stakeAmount / 2 ))
+
+function getDailyGamblingResult () {
+	while (( $stakeAmount > $lowerStackLimit && $stakeAmount < $upperStackLimit ))
+	do
+		winLoss=$(( RANDOM % 2 ))
+		case $winLoss in
+			$WIN )
+				((stakeAmount++));;
+			$LOSS )
+				((stakeAmount--));;
+		esac
+	done
+	echo $stakeAmount
 }
 
-getWinLoss
+function main () {
+	stakeAmount=$( getDailyGamblingResult )
+	echo "$stakeAmount"
+}
+
+main
