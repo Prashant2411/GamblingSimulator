3a8a344c78606912180a499f5fc57232af8919dc
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-11 11:38:15 +0530

    Added: Functionality to check win or loss

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -3,4 +3,18 @@
 echo "Welcome to the Gambling Simulator."
 
 BET_AMOUNT=1
+WIN=1
+LOSS=0
 STAKE_AMOUNT=100
+
+function getWinLoss () {
+	winLoss=$(( RANDOM%2 ))
+	case $winLoss in
+	$WIN )
+		echo "Win";;
+	$LOSS )
+		echo "Loss";;
+	esac
+}
+
+getWinLoss
